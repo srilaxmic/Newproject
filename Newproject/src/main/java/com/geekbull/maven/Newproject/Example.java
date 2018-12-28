2cdcb17e7a8bc42f6df5f375e48456d77adf2d88
srilaxmic <csrilaxmi154@gmail.com>
2018-12-28 13:09:02 +0530

    first commit

---
+++ Newproject/src/main/java/com/geekbull/maven/Newproject/Example.java
@@ -0,0 +1,8 @@
+package com.geekbull.maven.Newproject;
+
+public class Example {
+	public static void main(String[] args) {
+System.out.println("abc");
+System.out.println("xyz");
+}
+}
---
+++ Newproject/src/main/java/com/geekbull/maven/Newproject/Operators.java
@@ -0,0 +1,15 @@
+package com.geekbull.maven.Newproject;
+
+public class Operators {
+int a,b,c;
+
+	public static void main(String[] args) {
+		
+		System.out.println("* ");
+		System.out.println("+ ");
+		System.out.println("-");
+		
+
+	}
+
+}
